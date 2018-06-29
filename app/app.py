8baa04567a40ee66d2e7f320d34564f5408df9c1
Eric OC <eric@ericoc.com>
2018-06-29 19:31:48 +0000

    make chart actually work with terrible mysql query method

--- app/app.py
+++ app/app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, make_response
 from indego import Indego
+import _mysql
+import db_creds
 
 app = Flask(__name__)
+
+def check_kiosk_id(kiosk_id=None):
+
+    if kiosk_id and kiosk_id.isdigit() and len(kiosk_id) == 4:
+        indego = Indego()
+        find_kiosk_id = indego.get_stations(kiosk_id)
+
+        if find_kiosk_id and len(find_kiosk_id) == 1:
+             return find_kiosk_id
+
+    return None
 
 
 def jawn(search_string=None, emoji=False):
@@ -14,22 +27,45 @@ def jawn(search_string=None, emoji=False):
 
 def chart_jawn(kiosk_id=None):
 
-    indego = Indego()
-    find_station = indego.get_stations(kiosk_id)
+    chart_kiosk = check_kiosk_id(kiosk_id)
 
-    try:
-        if find_station and len(find_station) > 0:
+    if chart_kiosk:
+            station = chart_kiosk
             code = 200
-            station = find_station
-        else:
-            code = 404
+    else:
             station = None
-    except TypeError:
+            code = 404
+
+    return render_template('chart.html.j2', indego_stations=list(station.values())), code
+
+
+def chartjs_jawn(kiosk_id=None):
+
+    chartjs_kiosk = check_kiosk_id(kiosk_id)
+
+    if chartjs_kiosk:
+        chartjs_data = fetch_chart_data(kiosk_id)
+        code = 200
+    else:
+        chartjs_data = None
         code = 404
-        station = None
 
-    return render_template('chart.html.j2', station=station), code
+    chartjs_response = render_template('chart.js.j2', indego_station=list(chartjs_kiosk.values()), station_data=chartjs_data), code
+    response = make_response(chartjs_response)
+    response.headers['Content-Type'] = 'text/javascript'
+    return response
 
+
+def fetch_chart_data(kiosk_id):
+
+#    SELECT UNIX_TIMESTAMP(`added`)*1000 AS `added`, `bikesAvailable` FROM `data` WHERE `kioskId` = :kioskId AND `added` > NOW() - INTERVAL 1 MONTH ORDER BY `added` ASC;
+
+    db = _mysql.connect(host=db_creds.db_creds['host'], user=db_creds.db_creds['user'], passwd=db_creds.db_creds['passwd'], db=db_creds.db_creds['db'])
+    chart_query = "SELECT UNIX_TIMESTAMP(`added`)*1000 AS `added`, `bikesAvailable` FROM `data` WHERE `kioskId` = " + kiosk_id + " AND `added` > NOW() - INTERVAL 1 MONTH ORDER BY `added` ASC;"
+    db.query(chart_query)
+    result = db.store_result().fetch_row(how=1, maxrows=0)
+    db = None;
+    return result;
 
 @app.route('/')
 def index():
@@ -42,6 +78,10 @@ def search_stations(search_string):
 @app.route('/chart/<chart_id>')
 def chart_station(chart_id):
     return chart_jawn(kiosk_id=chart_id)
+
+@app.route('/chartjs/<chartjs_id>')
+def chartjs_station(chartjs_id):
+    return chartjs_jawn(kiosk_id=chartjs_id)
 
 @app.route('/favicon.ico')
 @app.route('/icon.png')
