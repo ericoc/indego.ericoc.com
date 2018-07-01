a61a6c3828b56bc64ca68e9a90ca21e3a65a369c
Eric OC <eric@ericoc.com>
2018-07-01 19:18:27 +0000

    use url_for, move functions in the app routes where i can, clean up the data retrieval for charts a bit

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request, make_response
+from flask import Flask, render_template, send_from_directory, request, make_response, url_for
 from indego import Indego
 import _mysql
 import db_creds
@@ -24,12 +24,31 @@ def home(search_string=None, emoji=False):
     return render_template('index.html.j2', indego_stations=search_results, emoji=emoji)
 
 
-def chart_web(chart_string=None):
+def fetch_chart_data(fetch_data_id):
 
-    if chart_string:
-        chart_results = check_kiosk_id(chart_string)
-    else:
-        chart_results = None
+    if not fetch_data_id or not len(fetch_data_id) == 4 or not check_kiosk_id(fetch_data_id):
+        return None
+
+    fetch_data_query = "SELECT UNIX_TIMESTAMP(`added`)*1000 AS `added`, `bikesAvailable` FROM `data` WHERE `kioskId` = " + fetch_data_id + " AND `added` > NOW() - INTERVAL 1 MONTH ORDER BY `added` ASC;"
+    chart_db = _mysql.connect(host=db_creds.db_creds['host'], user=db_creds.db_creds['user'], passwd=db_creds.db_creds['passwd'], db=db_creds.db_creds['db'])
+    chart_db.query(fetch_data_query)
+    chart_db_result = chart_db.store_result().fetch_row(how=1, maxrows=0)
+    chart_db = None;
+    return chart_db_result;
+
+
+@app.route('/')
+def index():
+    return home()
+
+@app.route('/search/<search_string>')
+def search_stations(search_string=None):
+    return home(search_string)
+
+@app.route('/chart/<chart_string>')
+def chart_station(chart_string=None):
+
+    chart_results = check_kiosk_id(chart_string)
 
     if chart_results:
             chart_stations = list(chart_results.values())
@@ -41,72 +60,46 @@ def chart_web(chart_string=None):
     return render_template('chart.html.j2', chart_stations=chart_stations, chart_string=chart_string), code
 
 
-def chartjs_web(kiosk_id=None):
+@app.route('/chartjs/<chartjs_id>')
+def chartjs_station(chartjs_id=None):
 
-    chartjs_kiosk = check_kiosk_id(kiosk_id)
+    chartjs_results = check_kiosk_id(chartjs_id)
 
-    if chartjs_kiosk:
-        indego_stations = list(chartjs_kiosk.values())
+    if chartjs_results:
+        chartjs_stations = list(chartjs_results.values())
         code = 200
     else:
-        indego_stations = None
+        chartjs_stations = None
         code = 404
 
-    chartjs_response = render_template('chart.js.j2', indego_stations=indego_stations), code
-    response = make_response(chartjs_response)
-    response.headers['Content-Type'] = 'text/javascript'
-    return response
+    chartjs_template = render_template('chart.js.j2', chartjs_stations=chartjs_stations), code
+    chartjs_response = make_response(chartjs_template)
+    chartjs_response.headers['Content-Type'] = 'text/javascript'
+    return chartjs_response
 
 
-def chartdata_web(kiosk_id=None):
+@app.route('/chartdata/<chartdata_id>')
+def chartdata_station(chartdata_id=None):
 
-    chartdata_kiosk = check_kiosk_id(kiosk_id)
+    if not chartdata_id or not len(chartdata_id) == 4:
+        chartdata_result = None
+    else:
+        chartdata_result = check_kiosk_id(chartdata_id)
 
-    if chartdata_kiosk:
-        indego_station = list(chartdata_kiosk.values())
-        chart_data = fetch_chart_data(kiosk_id)
+    if chartdata_result:
+        chartdata_station = list(chartdata_result.values())
+        chart_data = fetch_chart_data(chartdata_id)
         code = 200
     else:
-        indego_station = None
+        chartdata_station = None
         chart_data = None
         code = 404
 
-    chartdata_response = render_template('chart_data.js.j2', indego_station=indego_station, chart_data=chart_data), code
+    chartdata_response = render_template('chartdata.js.j2', chartdata_station=chartdata_station, chart_data=chart_data), code
     response = make_response(chartdata_response)
     response.headers['Content-Type'] = 'text/javascript'
     return response
 
-
-def fetch_chart_data(kiosk_id):
-
-#    SELECT UNIX_TIMESTAMP(`added`)*1000 AS `added`, `bikesAvailable` FROM `data` WHERE `kioskId` = :kioskId AND `added` > NOW() - INTERVAL 1 MONTH ORDER BY `added` ASC;
-
-    db = _mysql.connect(host=db_creds.db_creds['host'], user=db_creds.db_creds['user'], passwd=db_creds.db_creds['passwd'], db=db_creds.db_creds['db'])
-    chart_query = "SELECT UNIX_TIMESTAMP(`added`)*1000 AS `added`, `bikesAvailable` FROM `data` WHERE `kioskId` = " + kiosk_id + " AND `added` > NOW() - INTERVAL 1 MONTH ORDER BY `added` ASC;"
-    db.query(chart_query)
-    result = db.store_result().fetch_row(how=1, maxrows=0)
-    db = None;
-    return result;
-
-@app.route('/')
-def index():
-    return home()
-
-@app.route('/search/<search_string>')
-def search_stations(search_string):
-    return home(search_string)
-
-@app.route('/chart/<chart_string>')
-def chart_station(chart_string):
-    return chart_web(chart_string)
-
-@app.route('/chartjs/<chartjs_id>')
-def chartjs_station(chartjs_id):
-    return chartjs_web(kiosk_id=chartjs_id)
-
-@app.route('/chartdata/<chartdata_id>')
-def chartdata_station(chartdata_id):
-    return chartdata_web(kiosk_id=chartdata_id)
 
 @app.route('/favicon.ico')
 @app.route('/icon.png')
