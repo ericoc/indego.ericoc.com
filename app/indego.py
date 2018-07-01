98aebbdc0ca92de132785288af1c47df6276d7c5
Eric OC <eric@ericoc.com>
2018-07-01 18:06:41 +0000

    tabs to spaces and fix my bug from upstream

--- app/indego.py
+++ app/indego.py
@@ -10,75 +10,76 @@ Create a class to work with the Philadelphia Indego Bike Share API
 class Indego():
 
 
-	"""Create an init function to run on instantiation of the class"""
-	def __init__(self):
+    """Create an init function to run on instantiation of the class"""
+    def __init__(self):
 
-		# Create an empty dictionary to fill in with station data
-		self.stations = {}
+        # Create an empty dictionary to fill in with station data
+        self.stations = {}
 
-		# Initialization (retrieval) of station data has not happened yet
-		self.initialized = False
-
-
-	"""Create a function to hit the API and find all of the stations"""
-	def __find_stations(self):
-
-		# Specify the Indego bikes API URL, a friendly user-agent, and custom headers to hit it with
-		url = 'https://www.rideindego.com/stations/json/'
-		user_agent = 'Indego Python3 API Library - https://github.com/ericoc/indego-py-lib'
-		headers = {'Accept': 'application/json', 'User-Agent': user_agent}
-
-		# Hit the API and decode the JSON response
-		data = requests.get(url, headers=headers)
-		json = data.json()
-
-		# Add each station to our own dictionary that is easier to work with
-		for station in json['features']:
-			self.stations[station['properties']['kioskId']] = station['properties']
-
-		# Initialization is complete since stations have been found at this point
-		self.initialized = True
+        # Initialization (retrieval) of station data has not happened yet
+        self.initialized = False
 
 
-	"""Create a function to search for and return stations"""
-	def get_stations(self, where):
+    """Create a function to hit the API and find all of the stations"""
+    def __find_stations(self):
 
-		# Find all of the stations first, if that has not already been done
-		if not self.initialized:
-			self.__find_stations()
+        # Specify the Indego bikes API URL, a friendly user-agent, and custom headers to hit it with
+        url = 'https://www.rideindego.com/stations/json/'
+        user_agent = 'Indego Python3 API Library - https://github.com/ericoc/indego-py-lib'
+        headers = {'Accept': 'application/json', 'User-Agent': user_agent}
 
-		# Create empty dictionary to fill in with station data that will be returned by this function
-		out = {}
+        # Hit the API and decode the JSON response
+        data = requests.get(url, headers=headers)
+        json = data.json()
 
-		# Just provide all of the stations if no search query was given
-		if not where:
-			out = self.stations
+        # Add each station to our own dictionary that is easier to work with
+        for station in json['features']:
+            self.stations[station['properties']['kioskId']] = station['properties']
 
-		# If a search query was passed, process it...
-		else:
+        # Initialization is complete since stations have been found at this point
+        self.initialized = True
 
-			# Loop through each station in the primary dictionary
-			for station in self.stations:
 
-				# If the search query is numeric, it could either be a zip code or a kiosk ID
-				if where.isdigit():
+    """Create a function to search for and return stations"""
+    def get_stations(self, where):
 
-					# Kiosk IDs are four digits (so far... new stations could break this eventually)
-					# A kiosk ID match only returns that single station immediately
-					# The kioskID from the API is an integer so it has to be compared that way
-					if len(where) == 4:
-						if self.stations[station]['kioskId'] == int(where):
-							out[station] = self.stations[station]
-							return out
+        # Find all of the stations first, if that has not already been done
+        if not self.initialized:
+            self.__find_stations()
 
-					# Zip codes are five digits
-					elif len(where) == 5:
-						if self.stations[station]['addressZipCode'] == where:
-							out[station] = self.stations[station]
+        # Create empty dictionary to fill in with station data that will be returned by this function
+        out = {}
 
-				# Do a regular expression match against the name and address of each station
-				if (re.search(where, self.stations[station]['addressStreet'], re.IGNORECASE)) or (re.search(where, self.stations[station]['name'], re.IGNORECASE)):
-					out[station] = self.stations[station]
+        # Just provide all of the stations if no search query was given
+        if not where:
+            out = self.stations
 
-		# Return the stations!
-		return out
+        # If a search query was passed, process it...
+        else:
+
+            # Loop through each station in the primary dictionary
+            for station in self.stations:
+
+                # If the search query is numeric, it could either be a zip code or a kiosk ID
+                if where.isdigit():
+
+                    # Kiosk IDs are four digits (so far... new stations could break this eventually)
+                    # A kiosk ID match only returns that single station immediately
+                    # The kioskID from the API is an integer so it has to be compared that way
+                    if len(where) == 4:
+                        if self.stations[station]['kioskId'] == int(where):
+                            out = {}
+                            out[station] = self.stations[station]
+                            return out
+
+                    # Zip codes are five digits
+                    elif len(where) == 5:
+                        if self.stations[station]['addressZipCode'] == where:
+                            out[station] = self.stations[station]
+
+                # Do a regular expression match against the name and address of each station
+                if (re.search(where, self.stations[station]['addressStreet'], re.IGNORECASE)) or (re.search(where, self.stations[station]['name'], re.IGNORECASE)):
+                    out[station] = self.stations[station]
+
+        # Return the stations!
+        return out
