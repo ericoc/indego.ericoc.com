3a3befec28ef6eb3009bd14282c27bcbc7e37200
Eric OC <eric@ericoc.com>
2019-04-29 14:09:30 +0000

    use named parameters for mysql inserts/updates

--- app/db_insert.py
+++ app/db_insert.py
@@ -17,20 +17,36 @@ try:
 
             print("%s / %s" % (indego_station['kioskId'], indego_station['name']))
 
+            station_values = {
+                'kioskId' : indego_station['kioskId'],
+                'name' : indego_station['name'],
+                'addressStreet' : indego_station['addressStreet'],
+                'addressCity' : indego_station['addressCity'],
+                'addressState' : indego_station['addressState'],
+                'addressZipCode' : indego_station['addressZipCode']
+            }
+
             print("Station: ", end='')
             insert_station = ("INSERT INTO `stations` (`kioskId`, `name`, `addressStreet`, `addressCity`, `addressState`, `addressZipCode`, `added`)"
-                              "VALUES (%s, %s, %s, %s, %s, %s, NOW())"
-                              "ON DUPLICATE KEY UPDATE `name` = %s, `addressStreet` = %s, `addressCity` = %s, `addressState` = %s, `addressZipCode` = %s")
+                              "VALUES (%(kioskId)s, %(name)s, %(addressStreet)s, %(addressCity)s, %(addressState)s, %(addressZipCode)s, NOW())"
+                              "ON DUPLICATE KEY UPDATE `name` = %(name)s, `addressStreet` = %(addressStreet)s, `addressCity` = %(addressCity)s, `addressState` = %(addressState)s, `addressZipCode` = %(addressZipCode)s")
 
-            station_values = (indego_station['kioskId'], indego_station['name'], indego_station['addressStreet'], indego_station['addressCity'], indego_station['addressState'], indego_station['addressZipCode'], indego_station['name'], indego_station['addressStreet'], indego_station['addressCity'], indego_station['addressState'], indego_station['addressZipCode'])
             dbc.execute(insert_station, station_values)
             print('OK')
 
+
+            data_values = {
+                'kioskId' : indego_station['kioskId'],
+                'kioskPublicStatus' : indego_station['kioskPublicStatus'],
+                'bikesAvailable' : indego_station['bikesAvailable'],
+                'docksAvailable' : indego_station['docksAvailable'],
+                'totalDocks' : indego_station['totalDocks']
+            }
+
             print("Data: ", end='')
             insert_data = ("INSERT INTO `data` (`kioskId`, `kioskPublicStatus`, `bikesAvailable`, `docksAvailable`, `totalDocks`, `added`)"
-                           "VALUES (%s, %s, %s, %s, %s, NOW())")
+                           "VALUES (%(kioskId)s, %(kioskPublicStatus)s, %(bikesAvailable)s, %(docksAvailable)s, %(totalDocks)s, NOW())")
 
-            data_values = (indego_station['kioskId'], indego_station['kioskPublicStatus'], indego_station['bikesAvailable'], indego_station['docksAvailable'], indego_station['totalDocks'])
             dbc.execute(insert_data, data_values)
             print('OK')
 
