8c6c11eda6fdbc78e8ad7d10b5935b401bb50c63
Eric O'Callaghan <eric@ericoc.com>
2023-03-22 21:12:51 -0400

    add comments to insert script

--- psql_insert.py
+++ psql_insert.py
@@ -12,25 +12,31 @@ import requests
 from database import db_session
 from models import Indego
 
-
+# Configure logging
 logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S %Z', level=logging.INFO,
     format='%(asctime)s [%(levelname)s] (%(process)d): %(message)s',
     handlers=[logging.StreamHandler()]
 )
 
+# Make a request to the Indego bike-share API
 indego = requests.get(
     headers={'Accept': 'application/json',
              'User-Agent': 'Indego Python3 API Library - https://github.com/ericoc/indego-py-lib'},
     timeout=30, url='https://kiosks.bicycletransit.workers.dev/phl'
 )
 
+# Proceed if it was a successful response
 if indego.status_code == 200:
+
+    # Create a new PostgreSQL database row,
+    #   using the data from the JSON API response
     new = Indego(data=indego.json())
     db_session.add(new)
     db_session.commit()
     logging.info('OK')
     sys.exit(0)
 
+# Log and exit if something went wrong
 logging.fatal(indego)
 sys.exit(1)
